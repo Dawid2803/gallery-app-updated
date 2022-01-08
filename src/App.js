f6708f93bbd32e02d438750156771995840808ae
Dawid2803 <dawidjacoba@gmail.com>
2022-01-08 11:16:50 +0200

    fetched flickr data with axios

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+
+// Component imports
 import Search from "./components/Search";
 import Nav from "./components/Nav";
 import PhotoContainer from "./components/PhotoContainer";
+import apiKey from "./config"
 
-function App() {
-  return (
-    <div>
-      <Search />
-      <Nav />
-      <PhotoContainer />
-      
-    </div>
-  );
+
+export class App extends Component {
+
+  state = {
+    galleryData: []
+  }
+
+
+
+  componentDidMount(){
+    this.performSearch()
+  }
+
+  performSearch = () => {
+    axios.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=09c4b273fe9136275a25231194314587&tags=cats&per_page=24&format=json&nojsoncallback=1')
+    .then(resData => {
+      this.setState({galleryData: resData.data.photos.photo})
+      console.log(this.state.galleryData)
+    })
+  }
+
+  
+  render(){
+
+
+    return (
+      <div>
+        <Search />
+        <Nav />
+        <PhotoContainer />
+      </div>
+      );
+  }
 }
 
 export default App;
