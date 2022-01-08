a29a3da37e20c8a6c90dd95f0d25d1cc5d7feea9
Dawid2803 <dawidjacoba@gmail.com>
2022-01-08 12:03:48 +0200

    imported apiKey with config file

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import axios from 'axios'
 import Search from "./components/Search";
 import Nav from "./components/Nav";
 import PhotoContainer from "./components/PhotoContainer";
-import apiKey from "./config"
-
+import { apiKey } from './config'
 
 export class App extends Component {
 
@@ -15,14 +14,15 @@ export class App extends Component {
     galleryData: []
   }
 
+   
 
 
   componentDidMount(){
     this.performSearch()
   }
 
-  performSearch = (tags = 'dogs') => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=09c4b273fe9136275a25231194314587&tags=${tags}&per_page=24&format=json&nojsoncallback=1`)
+  performSearch = (tags = 'birds') => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${tags}&per_page=24&format=json&nojsoncallback=1`)
     .then(resData => {
       this.setState({galleryData: resData.data.photos.photo})
       console.log(this.state.galleryData)
