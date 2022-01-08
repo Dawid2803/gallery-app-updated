ed2eb83c6dbfe917a483962647ab2bd89a675a16
Dawid2803 <dawidjacoba@gmail.com>
2022-01-08 09:50:10 +0200

    started with App and NotFound component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Search from "./components/Search";
 import Nav from "./components/Nav";
+import PhotoContainer from "./components/PhotoContainer";
 
 function App() {
   return (
     <div>
       <Search />
       <Nav />
+      <PhotoContainer />
+      
     </div>
   );
 }
